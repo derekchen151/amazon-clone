74a9f78200b854213d8f6d009a9798686013553b
derek chen <derek.chen@fleetcomplete.com>
2020-12-25 10:09:28 -0500

    initial commit with header

---
+++ src/Header.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Header.scss'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+function header() {
+    return (
+        <div className="header">
+            <img className="header__logo" 
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+                alt="Amazon Logo"/>
+            <div className="header__search">
+                <input type="text" className="header__search__input"/>
+                <SearchIcon className="header__search__icon"></SearchIcon>
+            </div>
+            <div className="header__nav">
+                <div className="header__nav__option">
+                    <span className="header__nav__option__one">
+                        Hello Guest
+                    </span>
+                    <span className="header__nav__option__two">
+                        Sign In
+                    </span>
+                </div>
+                <div className="header__nav__option">
+                    <span className="header__nav__option__one">
+                        Returns
+                    </span>
+                    <span className="header__nav__option__two">
+                        & Orders
+                    </span>
+                </div>
+                <div className="header__nav__option">
+                    <span className="header__nav__option__one">
+                        Your
+                    </span>
+                    <span className="header__nav__option__two">
+                        Prime
+                    </span>
+                </div>
+                <div className="header__basket">
+                    <ShoppingBasketIcon />
+                    <span className="header__nav__option__two header__basket__count">0</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDNl2SDM1QDTAhonUUx_x_OgfHh2vViT-c",
+    authDomain: "clone-17068.firebaseapp.com",
+    projectId: "clone-17068",
+    storageBucket: "clone-17068.appspot.com",
+    messagingSenderId: "773789042070",
+    appId: "1:773789042070:web:ff25a09e90fa62d3ce09b7",
+    measurementId: "G-PJN8BGZRX6"
+  };
