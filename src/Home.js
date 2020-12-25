2eaf6570b9f5017186f14178de2ba14b4d693399
derek chen <derek.chen@fleetcomplete.com>
2020-12-25 13:13:40 -0500

    add home component

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Home.scss';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__container__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="Prime Video Banner"/>
+            </div>
+
+            <div className="home__row">
+                
+            </div>
+
+            <div className="home__row">
+
+            </div>
+            
+            <div className="home__row">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
