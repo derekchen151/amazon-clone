e68631304c89b6669c923892dac834bce36d3880
derek chen <derek.chen@fleetcomplete.com>
2020-12-27 13:16:36 -0500

    add stateprovider and data layer

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import './Product.scss';
 import StarIcon from '@material-ui/icons/Star';
+import { useStateValue } from '../../StateProvider';
 
-function Product({title, image, price, rating}) {
+function Product({id, title, image, price, rating}) {
+    const [{ basket }, dispatch] = useStateValue();
+
+    console.log(basket);
+
+    const addToBasket = () => {
+        dispatch ({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
+    };
+
     return (
         <div className="product">
             <div className="product__info">
@@ -18,7 +36,7 @@ function Product({title, image, price, rating}) {
                 </div>
             </div>
             <img src={image} alt={title} className="product__info__image"/>
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
